25d84f58ecf5dce9059905084d10a4aa56189365
Dipesh Wagle <dipesh.wagle@inficare.com.my>
2025-02-12 10:14:39 +0545

    Global expection handling

---
+++ CafeMgmtSystem/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace CafeMgmtSystem
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var errorResponse = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred. Please try again later.",
+                Detailed = exception.Message // You can remove this in production for security reasons
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(errorResponse);
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
